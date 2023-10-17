e31074b0d6118d42d78e1ddf6fd0544339bd2420
Romjan Hossain <romjanhossainn@gmail.com>
2023-10-18 01:24:30 +0600

    fashion frontend work start

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGcv1tMQkJlRxXxXsGsEE2pR9SwFjh8-U",
+  authDomain: "fashion-apparel.firebaseapp.com",
+  projectId: "fashion-apparel",
+  storageBucket: "fashion-apparel.appspot.com",
+  messagingSenderId: "485142785137",
+  appId: "1:485142785137:web:f3381a5a60637f80197fd0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
